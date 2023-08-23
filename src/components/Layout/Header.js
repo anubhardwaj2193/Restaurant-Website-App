62d33bd1c0946d0bed4919d07c516ddc3e520336
anubhardwaj2193 <anubhardwaj2193>
2023-08-24 00:48:12 +0530

    clickOnCart

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -7,7 +7,7 @@ return (
 <Fragment>
     <header className={classes.header}>
         <h1>ReactMeals</h1>
-        <HeaderCartButton/>
+        <HeaderCartButton onClick={props.onShowCart} />
         
     </header>
     <div className={classes['main-image']}>
--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -2,10 +2,10 @@ import { Fragment } from 'react';
 import ReactDOM from 'react-dom';
 
 import classes from './Modal.module.css';
-import {createPortal} from 'react-dom';
+
 
 const Backdrop = (props) => {
-  return <div className={classes.backdrop} />;
+  return <div className={classes.backdrop} onClick={props.onClose}/>;
 };
 
 const ModalOverlay = (props) => {
@@ -21,7 +21,7 @@ const portalElement = document.getElementById('overlays');
 const Modal = (props) => {
   return (
     <Fragment>
-      {ReactDOM.createPortal(<Backdrop />, portalElement)}
+      {ReactDOM.createPortal(<Backdrop onClose={props.onClose} />, portalElement)}
       {ReactDOM.createPortal(
         <ModalOverlay>{props.children}</ModalOverlay>,
         portalElement
